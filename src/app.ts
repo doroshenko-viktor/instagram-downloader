29781191621184a38a1af45f47e70c41ae988d4f
Viktor Doroshenko <reg.doroshenko@gmail.com>
2021-12-20 17:47:35 +0200

    test cli command

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('ff');
+#! /usr/bin/env node
+import { program } from "commander";
+import chalk from "chalk";
+
+const action = () => {
+    console.log(chalk.blue.italic("action executing"));
+    console.log(chalk.red.bold("formatted message"));
+};
+
+program.command('test').description('test option').action(action);
+
+program.parse();
